d83466999fdcaf4716e52e58ccaf5e2af1abdd7b
Vasilis Kalintiris <vasilis@netdata.cloud>
2021-02-10 18:00:06 +0200

    Bump

--- daemon/ml.c
+++ daemon/ml.c
@@ -74,6 +74,7 @@ RRDR *get_rrdr(RRDSET *set, time_t time_after, time_t time_before) {
         fatal("Row diff = %zu", row_diff);
 
     info("result contains %ld rows", res->rows);
+#ifdef KMEANS_CHECKS
     for (long i = 0; i != res->rows; i++) {
         calculated_number *cn = &res->v[res->d * i];
         RRDR_VALUE_FLAGS *vf = &res->o[res->d * i];
@@ -82,17 +83,38 @@ RRDR *get_rrdr(RRDSET *set, time_t time_after, time_t time_before) {
             if (vf[j] && RRDR_VALUE_EMPTY)
                 fatal("Found empty value!");
     }
+#endif
 
     return res;
 }
 
+static void dump_stats(usec_t sb_dt, usec_t km_dt, usec_t total_dt,
+                       size_t num_samples) {
+    FILE *fp = fopen("/tmp/stats.log", "a");
+    if (!fp)
+        fatal("Could not open stats log file");
+
+    fprintf(fp, "%zu %zu %zu %zu %zu %Lu %Lu %Lu\n",
+            num_samples, num_dims_per_sample,
+            diff_n, smooth_n, lag_n,
+            sb_dt / USEC_PER_MS,
+            km_dt / USEC_PER_MS,
+            total_dt / USEC_PER_MS);
+
+    fflush(fp);
+    fclose(fp);
+}
+
 void foobar(const char *hostname, time_t time_after, time_t time_before) {
+    struct timeval tv_begin, tv_end;
+    struct timeval tv_sb_begin, tv_sb_end;
+    struct timeval tv_km_begin, tv_km_end;
+
+    now_monotonic_high_precision_timeval(&tv_begin);
+    now_monotonic_high_precision_timeval(&tv_sb_begin);
+
     RRDHOST *host = rrdhost_find_by_hostname(hostname, simple_hash(hostname));
     RRDSET *set = rrdset_find_byname(host, "example_local1.random");
-
-    info("Host: %s, Set: %s",
-         host->hostname ? host->hostname : "unnamed",
-         set->name ? set->name : "unnamed");
 
     RRDR *res = get_rrdr(set, time_after, time_before);
 
@@ -103,6 +125,9 @@ void foobar(const char *hostname, time_t time_after, time_t time_before) {
     calculated_number *cns = callocz(num_samples, bytes_per_feature);
     memcpy(cns, res->v, sizeof(calculated_number) * num_dims_per_sample * num_samples);
 
+    now_monotonic_high_precision_timeval(&tv_sb_end);
+
+#ifdef KMEANS_CHECKS
     for (long i = 0; i != res->rows; i++) {
         calculated_number *cno = &res->v[res->d * i];
         calculated_number *cnn = &cns[res->d * i];
@@ -111,11 +136,24 @@ void foobar(const char *hostname, time_t time_after, time_t time_before) {
             if (cno[j] != cnn[j])
                 fatal("cno[%ld][%ld] != cnn[%ld][%ld]: %Lf != %Lf", i, j, i, j, cno[j], cnn[j]);
     }
+#endif
 
+    now_monotonic_high_precision_timeval(&tv_km_begin);
     run_kmeans(cns, num_samples, num_dims_per_sample, diff_n, smooth_n, lag_n);
-
-    info("ALL DONE");
+    now_monotonic_high_precision_timeval(&tv_km_end);
 
     freez(cns);
     rrdr_free(res);
+
+    usec_t sb_dt = dt_usec(&tv_sb_end, &tv_sb_begin);
+    info("samples buffer time: %Lu msec", sb_dt / USEC_PER_MS);
+
+    usec_t km_dt = dt_usec(&tv_km_end, &tv_km_begin);
+    info("k-means time: %Lu msec", km_dt / USEC_PER_MS);
+
+    now_monotonic_high_precision_timeval(&tv_end);
+    usec_t total_dt = dt_usec(&tv_end, &tv_begin);
+    info("total time: %Lu msec", total_dt / USEC_PER_MS);
+
+    dump_stats(sb_dt, km_dt, total_dt, num_samples);
 }
--- run.sh
+++ run.sh
@@ -2,14 +2,20 @@
 
 set -exu -o pipefail
 
-find "$HOME/opt/netdata/var/cache/netdata/" -type f -delete
+NETDATA_ROOT="$HOME/opt/netdata"
 
 bear make install V=1 -j 8
 
-export NUM_SAMPLES=3600
-export NUM_DIMS_PER_SAMPLE=8
-export DIFF_N=1
-export SMOOTH_N=3
-export LAG_N=5
+for num_samples in $(seq 3600 3600 86400); do
+    for num_dims in $(seq 2 4 78); do
+        for i in $(seq 1 3); do
+            export NUM_DIMS_PER_SAMPLE=$num_dims
+            export DIFF_N=1
+            export SMOOTH_N=3
+            export LAG_N=5
 
-valgrind "$HOME/opt/netdata/usr/sbin/netdata" -W createdataset=$NUM_SAMPLES
+            find "$NETDATA_ROOT/var/cache/netdata/" -type f -delete
+            "$NETDATA_ROOT/usr/sbin/netdata" -W createdataset=$num_samples
+        done
+    done
+done
