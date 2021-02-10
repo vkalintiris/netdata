81152f1ebcdd2dd8bc9345684c5c8b6323120753
Vasilis Kalintiris <vasilis@netdata.cloud>
2021-02-10 12:57:09 +0200

    Create samples buffer

--- daemon/ml.c
+++ daemon/ml.c
@@ -8,6 +8,9 @@
 #define LAG_N 5
 
 RRDR *get_rrdr(RRDSET *set, time_t time_after, time_t time_before) {
+    if (time_after >= time_before)
+        fatal("time_after >= time_before (%ld >= %ld)", time_after, time_before);
+
     RRDR *res = rrd2rrdr(
         set,
         0, /* points_requested */
@@ -23,6 +26,14 @@ RRDR *get_rrdr(RRDSET *set, time_t time_after, time_t time_before) {
     if (!res) {
         fatal("RRDR result is empty\n");
     }
+
+    size_t max_possible_rows = time_before - time_after;
+    if (res->rows > max_possible_rows)
+        fatal("res->rows > max_possible_rows (%ld > %zu)", res->rows, max_possible_rows);
+
+    size_t row_diff = max_possible_rows - res->rows;
+    if (row_diff > 2)
+        fatal("Row diff = %zu", row_diff);
 
     info("result contains %ld rows", res->rows);
     for (long i = 0; i != res->rows; i++) {
@@ -46,6 +57,23 @@ void foobar(const char *hostname, time_t time_after, time_t time_before) {
          set->name ? set->name : "unnamed");
 
     RRDR *res = get_rrdr(set, time_after, time_before);
-    info("Got %ld rows back", res->rows);
+
+    size_t num_samples = res->rows;
+    size_t num_dims_per_sample = res->d;
+    size_t bytes_per_feature = sizeof(calculated_number) * num_dims_per_sample * (LAG_N + 1);
+
+    calculated_number *cns = callocz(num_samples, bytes_per_feature);
+    memcpy(cns, res->v, sizeof(calculated_number) * num_dims_per_sample * num_samples);
+
+    for (long i = 0; i != res->rows; i++) {
+        calculated_number *cno = &res->v[res->d * i];
+        calculated_number *cnn = &cns[res->d * i];
+
+        for (long j = 0; j != res->d; j++)
+            if (cno[j] != cnn[j])
+                fatal("cno[%ld][%ld] != cnn[%ld][%ld]: %Lf != %Lf", i, j, i, j, cno[j], cnn[j]);
+    }
+
+    freez(cns);
     rrdr_free(res);
 }
--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ set -exu -o pipefail
 
 find "$HOME/opt/netdata/var/cache/netdata/" -type f -delete
 
-bear make install V=1 -j 8 >/dev/null 2>&1
+bear make install V=1 -j 8
 
-valgrind "$HOME/opt/netdata/usr/sbin/netdata" -W createdataset=600
+valgrind "$HOME/opt/netdata/usr/sbin/netdata" -W createdataset=3600
