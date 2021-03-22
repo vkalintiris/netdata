a49fa02685f6736035fe53f0782e6d2398249877
Vasilis Kalintiris <vasilis@netdata.cloud>
2021-03-22 20:37:17 +0200

    Bump

--- ml/ml_train.cc
+++ ml/ml_train.cc
@@ -12,33 +12,65 @@ static void cleanupTrainThread(void *ptr) {
 
 namespace ml {
 
+usec_t processUnit(Unit *U, heartbeat_t &HB) {
+    struct timeval BTV, ETV;
+
+    now_monotonic_high_precision_timeval(&BTV);
+    U->train();
+    now_monotonic_high_precision_timeval(&ETV);
+
+    usec_t Duration = dt_usec(&BTV, &ETV);
+    usec_t UsecsPerUnit = (Cfg.TrainEvery * USEC_PER_SEC) / Cfg.NumUnits;
+
+    if (Duration < UsecsPerUnit)
+        heartbeat_next(&HB, UsecsPerUnit - Duration);
+
+    return std::max(Duration, UsecsPerUnit);
+}
+
 void trainMain(struct netdata_static_thread *Thread) {
     netdata_thread_cleanup_push(cleanupTrainThread, Thread);
 
     heartbeat_t HB;
     heartbeat_init(&HB);
 
+    usec_t UpdateHostsEvery = 10 * USEC_PER_SEC;
 
     while (!netdata_exit) {
+        // Update data structures.
         Cfg.updateHosts();
 
-        info("Num Units: %zu", Cfg.NumUnits);
+        // Track how much time we've spent in training.
+        usec_t TimeSpentTraining = 0;
 
-        for (auto &P : Cfg.Hosts) {
-            Host *H = P.second;
+        // For each host
+        for (auto &HP : Cfg.Hosts) {
+            Host *H = HP.second;
 
-            for (auto &P: H->ChartsMap) {
-                Chart *C = P.second;
+            // For each chart
+            for (auto &CP: H->ChartsMap) {
+                Chart *C = CP.second;
 
-                for (auto &P : C->UnitsMap) {
-                    Unit *U = P.second;
+                // For each unit
+                for (auto &UP : C->UnitsMap) {
+                    Unit *U = UP.second;
 
-                    U->train();
+                    if (!U->shouldTrain())
+                        continue;
+
+                    if (TimeSpentTraining < UpdateHostsEvery && !netdata_exit)
+                        TimeSpentTraining += processUnit(U, HB);
+
+                    info("TimeSpentTraining: %lld", TimeSpentTraining);
                 }
             }
         }
 
-        heartbeat_next(&HB, 10 * USEC_PER_SEC);
+        // Sleep if we have to.
+        if (TimeSpentTraining < UpdateHostsEvery && !netdata_exit) {
+            info("Sleeping for %lld usec\n", UpdateHostsEvery - TimeSpentTraining);
+            heartbeat_next(&HB, UpdateHostsEvery - TimeSpentTraining);
+        }
     }
 
     netdata_thread_cleanup_pop(1);
