6e5a69fdd4c1012fa7ef129082e8447d1f46f0cb
Andrew Maguire <andrewm4894@gmail.com>
2021-03-29 15:24:16 +0100

    add family level charts

--- collectors/python.d.plugin/anomaliespoc/anomaliespoc.chart.py
+++ collectors/python.d.plugin/anomaliespoc/anomaliespoc.chart.py
@@ -10,10 +10,18 @@ from bases.FrameworkServices.UrlService import UrlService
 priority = 85
 
 ORDER = [
-    'chart_probs', 'chart_flags'
+    'family_probs', 'family_flags', 'chart_probs', 'chart_flags'
 ]
 
 CHARTS = {
+    'family_probs': {
+        'options': [None, 'Family Probabilities', 'Family Probability', 'family probabilities', 'anomaliespoc.family_probability', 'line'],
+        'lines': []
+    },
+    'family_flags': {
+        'options': [None, 'Family Flags', 'Family Flag', 'family flags', 'anomaliespoc.family_flag', 'stacked'],
+        'lines': []
+    },
     'chart_probs': {
         'options': [None, 'Chart Probabilities', 'Chart Probability', 'chart probabilities', 'anomaliespoc.chart_probability', 'line'],
         'lines': []
@@ -21,7 +29,7 @@ CHARTS = {
     'chart_flags': {
         'options': [None, 'Chart Flags', 'Chart Flag', 'chart flags', 'anomaliespoc.chart_flag', 'stacked'],
         'lines': []
-    }
+    },
 }
 
 
@@ -34,6 +42,7 @@ class Service(UrlService):
         self.url = self.configuration.get('url', 'http://127.0.0.1:19999/api/v1/allmetrics?format=json')
         self.suffix = self.configuration.get('suffix', '_km')
         self.thold = self.configuration.get('thold', 90.0)
+        self.display_family = bool(self.configuration.get('display_family', True))
 
     def _get_data(self):
         raw_data = self._get_raw_data()
@@ -41,20 +50,38 @@ class Service(UrlService):
             return None
 
         raw_data = loads(raw_data)
+        chart_family_map = {c: raw_data[c]['family'] for c in raw_data}
+        chart_families = list(set(chart_family_map.values()))
         raw_data = {k: raw_data[k] for k in raw_data if k.endswith(self.suffix)}
 
+        # get chart level data
         chart_probs = {}
         chart_flags = {}
         for chart in raw_data:
             base_chart = chart.replace(self.suffix, '')
             anomaly_scores = [dim['value'] for dim in raw_data[chart]['dimensions'].values() if dim['value'] is not None]
             chart_probs[base_chart] = round(sum(anomaly_scores) / len(anomaly_scores), 2)
-            chart_flags["{}_flag".format(base_chart)] = max([1 if score >= self.thold else 0 for score in anomaly_scores])
-        
+            chart_flags["{}_flag".format(base_chart)] = max([1 if score >= self.thold else 0 for score in anomaly_scores])        
         self.update_charts('chart_probs', chart_probs)
         self.update_charts('chart_flags', chart_flags)
 
         data = {**chart_probs, **chart_flags}
+
+        # agg to family level
+        if self.display_family:
+            family_probs = {family: [] for family in chart_families}
+            family_flags = {family: [] for family in chart_families}
+            for chart in chart_probs:
+                family = chart_family_map.get(chart, None)
+                if family:
+                    family_probs[family].append(chart_probs[chart])
+                    family_flags[family].append(chart_flags[chart])
+            family_probs = {"{}_prob".format(f): round(sum(family_probs[f])/len(family_probs[f]), 2) for f in family_probs if len(family_probs[f]) > 0}
+            family_flags = {"{}_flag".format(f): round(sum(family_flags[f])/len(family_flags[f]), 2) for f in family_flags if len(family_flags[f]) > 0}
+            self.update_charts('family_probs', family_probs)
+            self.update_charts('family_flags', family_flags)
+
+            data = {**data, **family_probs, **family_flags}
 
         return data
 
