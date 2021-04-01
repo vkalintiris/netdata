9924941bea4173378514cbceafcbfc92a80ca507
Vasilis Kalintiris <vasilis@netdata.cloud>
2021-04-01 12:48:43 +0300

    Add simple pattern to exclude hosts from ML.

--- ml/ml.cc
+++ ml/ml.cc
@@ -25,12 +25,15 @@ void ml_init(void) {
         return;
 
     Cfg.UpdateEvery = Millis{15 * 1000};
-    Cfg.TrainSecs = Millis{config_get_number(CONFIG_SECTION_ML, "num secs to train", 60 * 60) * 1000};
-    Cfg.TrainEvery = Millis{config_get_number(CONFIG_SECTION_ML, "train every secs", 15 * 60) * 1000};
+    Cfg.TrainSecs = Millis{config_get_number(CONFIG_SECTION_ML, "num secs to train", 5 * 60) * 1000};
+    Cfg.TrainEvery = Millis{config_get_number(CONFIG_SECTION_ML, "train every secs", 1 * 60) * 1000};
 
     Cfg.DiffN = config_get_number(CONFIG_SECTION_ML, "num samples to diff", 1);
     Cfg.SmoothN = config_get_number(CONFIG_SECTION_ML, "num samples to smooth", 3);
     Cfg.LagN = config_get_number(CONFIG_SECTION_ML, "num samples to lag", 5);
+
+    std::string HostsToSkip = config_get(CONFIG_SECTION_ML, "hosts to skip from training", "!*");
+    Cfg.SP_HostsToSkip = simple_pattern_create(HostsToSkip.c_str(), NULL, SIMPLE_PATTERN_EXACT);
 
     std::string ChartsToSkip = config_get(CONFIG_SECTION_ML, "charts to skip from training", "!*");
     Cfg.SP_ChartsToSkip = simple_pattern_create(ChartsToSkip.c_str(), NULL, SIMPLE_PATTERN_EXACT);
