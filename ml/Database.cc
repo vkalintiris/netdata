879f308b9c1a3af292fbd7f02c84c3c90cef17c6
Vasilis Kalintiris <vasilis@netdata.cloud>
2021-04-05 15:01:20 +0300

    Add charts to track ML-related metrics

--- ml/Database.cc
+++ ml/Database.cc
@@ -8,6 +8,83 @@
 
 using namespace ml;
 
+static void updateMLStats(std::vector<Unit *> Units,
+                          usec_t PredictionDuration) {
+    /* Units stats chart */
+
+    static RRDSET *StatsRS = nullptr;
+    static RRDDIM *NumUnitsRD, *NumTrainedUnitsRD, *NumPredictedUnitsRD;
+
+    collected_number NumUnits, NumTrainedUnits, NumPredictedUnits;
+
+    if (unlikely(!StatsRS)) {
+        StatsRS = rrdset_create_localhost(
+                "ml",
+                "units",
+                NULL,
+                "ml_units",
+                NULL,
+                "Number of units trained/predicted",
+                "number of units",
+                "ml_units",
+                NULL,
+                39183,
+                1,
+                RRDSET_TYPE_LINE);
+
+        NumUnitsRD = rrddim_add(StatsRS, "total units",
+                                NULL, 1, 1, RRD_ALGORITHM_ABSOLUTE);
+        NumTrainedUnitsRD = rrddim_add(StatsRS, "trained units",
+                                       NULL, 1, 1, RRD_ALGORITHM_ABSOLUTE);
+        NumPredictedUnitsRD = rrddim_add(StatsRS, "predicted units",
+                                         NULL, 1, 1, RRD_ALGORITHM_ABSOLUTE);
+    } else {
+        rrdset_next(StatsRS);
+    }
+
+    NumUnits = Units.size();
+    rrddim_set_by_pointer(StatsRS, NumUnitsRD, NumUnits);
+
+    NumTrainedUnits = 0, NumPredictedUnits = 0;
+    for (const Unit *U : Units) {
+        NumTrainedUnits += U->isTrained() ? 1 : 0;
+        NumPredictedUnits += U->isPredicted() ? 1 : 0;
+    }
+    rrddim_set_by_pointer(StatsRS, NumTrainedUnitsRD, NumTrainedUnits);
+    rrddim_set_by_pointer(StatsRS, NumPredictedUnitsRD, NumPredictedUnits);
+
+    rrdset_done(StatsRS);
+
+    /* Prediction time chart */
+
+    static RRDSET *PredictionTimeRS = nullptr;
+    static RRDDIM *PredictionTimeRD;
+
+    if (unlikely(!PredictionTimeRS)) {
+        PredictionTimeRS = rrdset_create_localhost(
+                "ml",
+                "prediction_time",
+                NULL,
+                "prediction_time",
+                NULL,
+                "Time it took to predict units",
+                "barfoo",
+                "prediction_time",
+                NULL,
+                39184,
+                1,
+                RRDSET_TYPE_LINE);
+
+        PredictionTimeRD = rrddim_add(PredictionTimeRS, "prediction tread iteration time",
+                                      NULL, 1, USEC_PER_MS, RRD_ALGORITHM_ABSOLUTE);
+    } else {
+        rrdset_next(PredictionTimeRS);
+    }
+
+    rrddim_set_by_pointer(PredictionTimeRS, PredictionTimeRD, PredictionDuration);
+    rrdset_done(PredictionTimeRS);
+}
+
 void Database::updateHosts() {
     rrd_rdlock();
 
@@ -90,8 +167,6 @@ void Database::trainUnits() {
 
     std::vector<Unit *> Units = getUnits();
 
-    std::make_heap(Units.begin(), Units.end(), UnitComp());
-
     if (Units.size() == 0) {
         std::this_thread::sleep_for(Cfg.UpdateEvery);
         return;
@@ -123,12 +198,19 @@ void Database::trainUnits() {
 }
 
 void Database::predictUnits() {
+    static usec_t PredictionDuration;
+    struct timeval StartTV, EndTV;
+
+    now_monotonic_high_precision_timeval(&StartTV);
     {
         std::unique_lock<std::mutex> Lock(Mutex);
 
-        for (Unit *U : getUnits()) {
+        std::vector<Unit *> Units = getUnits();
+
+        for (Unit *U : Units)
             U->predict();
-        }
+
+        updateMLStats(Units, PredictionDuration);
     }
 
     {
@@ -139,9 +221,14 @@ void Database::predictUnits() {
 
             for (auto &CP: H->ChartsMap) {
                 Chart *C = CP.second;
-
                 C->updateMLChart();
             }
         }
     }
+    now_monotonic_high_precision_timeval(&EndTV);
+
+    PredictionDuration = dt_usec(&EndTV, &StartTV);
+
+    double Count = static_cast<double>(dt_usec(&EndTV, &StartTV)) / USEC_PER_SEC;
+    info("Prediction duration: %f", Count);
 }
