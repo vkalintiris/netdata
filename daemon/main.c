7069ffe12c456d4854b111945f47d8b57735a5f4
Vasilis Kalintiris <vasilis@netdata.cloud>
2021-09-14 12:50:31 +0300

    Boilerplate code to track hosts/dims and init ML config options.
    A new opaque pointer field is added to the database's host and dimension
    data structures. The fields point to C++ wrapper classes that will be used
    to store ML-related information in follow-up patches.
    
    The ML functionality needs to iterate all tracked dimensions twice per
    second. To avoid locking the entire DB multiple times, we use a
    separate dictionary to add/remove dimensions as they are created/deleted
    by the database.
    
    A global configuration object is initialized during the startup of the
    agent. It will allow our users to specify ML-related configuration
    options, eg. hosts/charts to skip from training, etc.

--- daemon/main.c
+++ daemon/main.c
@@ -1151,6 +1151,10 @@ int main(int argc, char **argv) {
         health_initialize_global_silencers();
 
         // --------------------------------------------------------------------
+        // Initialize ML configuration
+        ml_init();
+
+        // --------------------------------------------------------------------
         // setup process signals
 
         // block signals while initializing threads.
