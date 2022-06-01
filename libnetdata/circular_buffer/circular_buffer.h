e6a4e48c4ee3aa1f6d687b66b02f35adfe617766
vkalintiris <vasilis@netdata.cloud>
2022-06-01 11:39:45 +0300

    Add a function to get the remaining capacity of a circular buffer
    This will be used by the replication thread in order to fill the
    streaming's buffer with replication data only when its remaining
    capacity is big enough.

--- libnetdata/circular_buffer/circular_buffer.h
+++ libnetdata/circular_buffer/circular_buffer.h
@@ -1,6 +1,7 @@
 #ifndef CIRCULAR_BUFFER_H
 #define CIRCULAR_BUFFER_H 1
 
+#include <stdbool.h>
 #include <string.h>
 
 struct circular_buffer {
@@ -13,4 +14,9 @@ extern void cbuffer_free(struct circular_buffer *buf);
 extern int cbuffer_add_unsafe(struct circular_buffer *buf, const char *d, size_t d_len);
 extern void cbuffer_remove_unsafe(struct circular_buffer *buf, size_t num);
 extern size_t cbuffer_next_unsafe(struct circular_buffer *buf, char **start);
+
+// Return the remaining capacity of the circular buffer, ie. either from
+// the currently size of the buffer, or from the max size it can pontentionally
+// can grow to.
+extern size_t cbuffer_remaining_capacity(struct circular_buffer *buf, bool from_current_size);
 #endif
