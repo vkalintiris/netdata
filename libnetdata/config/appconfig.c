4b96a2b63cc10cb5f8a47d3801ab724d7a8ea5cc
vkalintiris <vasilis@netdata.cloud>
2022-06-01 13:18:16 +0300

    Add code to initialize/finalize the replication component
    For the time being, it's only being used to read the replication
    configuration options we need on startup. Ignore the actual
    option names/values because they will be modified in follow-up commits.

--- libnetdata/config/appconfig.c
+++ libnetdata/config/appconfig.c
@@ -844,13 +844,14 @@ void appconfig_generate(struct config *root, BUFFER *wb, int only_changed)
             else if(!strcmp(co->name, CONFIG_SECTION_ML))                pri = 7;
             else if(!strcmp(co->name, CONFIG_SECTION_HEALTH))            pri = 8;
             else if(!strcmp(co->name, CONFIG_SECTION_WEB))               pri = 9;
-            // by default, new sections will get pri = 10 (set at the end, below)
-            else if(!strcmp(co->name, CONFIG_SECTION_REGISTRY))          pri = 11;
-            else if(!strcmp(co->name, CONFIG_SECTION_GLOBAL_STATISTICS)) pri = 12;
-            else if(!strcmp(co->name, CONFIG_SECTION_PLUGINS))           pri = 13;
-            else if(!strcmp(co->name, CONFIG_SECTION_STATSD))            pri = 14;
-            else if(!strncmp(co->name, "plugin:", 7))                    pri = 15; // << change the loop too if you change this
-            else pri = 10; // this is used for any new (currently unknown) sections
+            else if(!strcmp(co->name, CONFIG_SECTION_REPLICATION))       pri = 10;
+            // by default, new sections will get pri = 11 (set at the end, below)
+            else if(!strcmp(co->name, CONFIG_SECTION_REGISTRY))          pri = 12;
+            else if(!strcmp(co->name, CONFIG_SECTION_GLOBAL_STATISTICS)) pri = 13;
+            else if(!strcmp(co->name, CONFIG_SECTION_PLUGINS))           pri = 14;
+            else if(!strcmp(co->name, CONFIG_SECTION_STATSD))            pri = 15;
+            else if(!strncmp(co->name, "plugin:", 7))                    pri = 16; // << change the loop too if you change this
+            else pri = 11; // this is used for any new (currently unknown) sections
 
             if(i == pri) {
                 int loaded = 0;
---
+++ streaming/replication/replication-private.h
@@ -0,0 +1,10 @@
+#ifndef REPLICATION_PRIVATE_H
+#define REPLICATION_PRIVATE_H
+
+#include "replication.h"
+
+#include <algorithm>
+
+#include "Config.h"
+
+#endif /* REPLICATION_PRIVATE_H */
