d67b9814b5b51ccee4c3d8d6a7e1396b3f492450
vkalintiris <vasilis@netdata.cloud>
2022-06-01 14:00:03 +0300

    Add protobuf messages used by replication.
    We only support autotools-based builds. CMake support will be added
    later.

--- streaming/replication/replication-private.h
+++ streaming/replication/replication-private.h
@@ -3,6 +3,16 @@
 
 #include "replication.h"
 
+#include "gaps.pb.h"
+
+#if GOOGLE_PROTOBUF_VERSION < 3001000
+#define PROTO_COMPAT_MSG_SIZE(msg) (size_t)msg.ByteSize()
+#define PROTO_COMPAT_MSG_SIZE_PTR(msg) (size_t)msg->ByteSize()
+#else
+#define PROTO_COMPAT_MSG_SIZE(msg) msg.ByteSizeLong()
+#define PROTO_COMPAT_MSG_SIZE_PTR(msg) msg->ByteSizeLong()
+#endif
+
 #include <algorithm>
 #include <chrono>
 #include <mutex>
