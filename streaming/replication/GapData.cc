4a083d779a02cc50bcc2501169e7c3c3f50522f7
vkalintiris <vasilis@netdata.cloud>
2022-06-04 17:09:33 +0300

    Replace the code in streaming/replication/collectops.{c,h}
    The new code is much simpler because we know that the after/before time
    of a gap can not be more than 1024 seconds apart.

--- streaming/replication/GapData.cc
+++ streaming/replication/GapData.cc
@@ -89,16 +89,100 @@ GapData GapData::fromBase64(const std::string &EncodedData) {
     return fromProto(PGD);
 }
 
+#ifdef ENABLE_DBENGINE
 bool GapData::flushToDBEngine(RRDHOST *RH) const {
-    RRDDIM_PAST_DATA *DPD;
-
-    DPD = replication_collect_past_metric_init(RH, Chart.c_str(), Dimension.c_str());
-    if (!DPD)
+    if (StorageNumbers.size() == 0) {
+        error("[%s] No storage numbers to flush to DBEngine for %s.%s",
+              RH->hostname, Chart.c_str(), Dimension.c_str());
         return false;
+    }
 
-    for (const auto &P : StorageNumbers)
-        replication_collect_past_metric(DPD, P.first, P.second);
+    /*
+     * Prepare the page's data that we want to write
+     */
 
-    replication_collect_past_metric_done(DPD);
+    constexpr time_t MaxEntriesPerPage = RRDENG_BLOCK_SIZE / sizeof(storage_number);
+    storage_number Page[MaxEntriesPerPage] = { 0 };
+
+    for (const auto &P : StorageNumbers) {
+        time_t Timestamp = P.first;
+        storage_number SN = P.second;
+
+        time_t Idx = Timestamp - StorageNumbers[0].first;
+        if (Idx < 0 || Idx >= MaxEntriesPerPage) {
+            error("[%s] Gap data index for %s.%s is not in [0, %ld] range (Idx=%ld)",
+                  RH->hostname, Chart.c_str(), Dimension.c_str(), MaxEntriesPerPage - 1, Idx);
+            return false;
+        }
+
+        Page[Idx] = SN;
+    }
+
+    /*
+     * Prepare dim past data structure
+     */
+
+    RRDDIM_PAST_DATA DPD;
+    memset(&DPD, 0, sizeof(DPD));
+
+    DPD.host = RH;
+    DPD.page = Page;
+    DPD.start_time = StorageNumbers[0].first;
+    DPD.end_time = StorageNumbers.back().first;
+    DPD.page_length = (DPD.end_time - DPD.start_time + 1) * sizeof(storage_number);
+
+    rrdhost_rdlock(RH);
+    DPD.st = rrdset_find(RH, Chart.c_str());
+    if (!DPD.st) {
+        error("[%s] Can not find chart %s", RH->hostname, Chart.c_str());
+        rrdhost_unlock(RH);
+        return false;
+    }
+
+    if (DPD.st->rrd_memory_mode != RRD_MEMORY_MODE_DBENGINE) {
+        error("[%s] Can not fill gap data because chart %s is not using dbengine", RH->hostname, Chart.c_str());
+        rrdhost_unlock(RH);
+        return false;
+    }
+
+    rrdset_rdlock(DPD.st);
+    DPD.rd = rrddim_find(DPD.st, Dimension.c_str());
+    if (!DPD.rd) {
+        error("[%s] Can not find dimension %s.%s", RH->hostname, Chart.c_str(), Dimension.c_str());
+        rrdset_unlock(DPD.st);
+        rrdhost_unlock(RH);
+        return false;
+    }
+
+    /*
+     * Write past data to dbengine
+     */
+
+    DPD.start_time *= USEC_PER_SEC;
+    DPD.end_time *= USEC_PER_SEC;
+
+    if (rrdeng_store_past_metrics_realtime(DPD.rd, &DPD)) {
+        if (rrdeng_store_past_metrics_page_init(&DPD)) {
+            fatal("Cannot initialize db engine page: Flushing collected past data skipped!");
+            rrdset_unlock(DPD.st);
+            rrdhost_unlock(RH);
+            return false;
+        }
+
+        rrdeng_store_past_metrics_page(&DPD);
+        rrdeng_flush_past_metrics_page(&DPD);
+        rrdeng_store_past_metrics_page_finalize(&DPD);
+        debug(D_REPLICATION, "[%s] Flushed gap data for %s.%s", RH->hostname, Chart.c_str(), Dimension.c_str());
+    }
+
+    rrdset_unlock(DPD.st);
+    rrdhost_unlock(RH);
     return true;
 }
+#else
+bool GapData::flushToDBEngine(RRDHOST *RH) const {
+    error("[%s] Can not fill gap data for %s.%s because the agent does not support DBEngine",
+          RH->hostname, Chart.c_str(), Dimension.c_str());
+    return false;
+}
+#endif
