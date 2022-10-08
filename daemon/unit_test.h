88c0be68d427d96d3a3d8df8ca7e9f5a66b623cb
Vasilis Kalintiris <vasilis@netdata.cloud>
2022-10-08 15:15:32 +0300

    Rm dead code

--- daemon/unit_test.h
+++ daemon/unit_test.h
@@ -1,16 +1,13 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 #ifndef NETDATA_UNIT_TEST_H
-#define NETDATA_UNIT_TEST_H 1
-
-int unit_test(long delay, long shift);
+#define NETDATA_UNIT_TEST_H
 
 #ifdef ENABLE_DBENGINE
 int test_dbengine(void);
 void generate_dbengine_dataset(unsigned history_seconds);
 void dbengine_stress_test(unsigned TEST_DURATION_SEC, unsigned DSET_CHARTS, unsigned QUERY_THREADS,
                                  unsigned RAMP_UP_SECONDS, unsigned PAGE_CACHE_MB, unsigned DISK_SPACE_MB);
-
 #endif
 
 #endif /* NETDATA_UNIT_TEST_H */
