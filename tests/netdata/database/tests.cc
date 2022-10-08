1cb75f32f5b49919f310694f6769b64fe4923767
Vasilis Kalintiris <vasilis@netdata.cloud>
2022-10-08 11:59:24 +0300

    port check_strdupz_path_subpath

--- tests/netdata/database/tests.cc
+++ tests/netdata/database/tests.cc
@@ -97,3 +97,30 @@ TEST(database, renaming) {
    std::string ChartName = J["chart_name"];
    EXPECT_EQ(ChartName, "chart.CHARTNAME2");
 }
+
+TEST(strdupz_path_subpath, test) {
+    struct PathParts {
+        const char *Path1;
+        const char *Path2;
+    };
+    std::vector<std::pair<PathParts, const char *>> Values {
+        { PathParts{"", ""}, "." },
+        { PathParts{"/", ""}, "/" },
+        { PathParts{"/etc/netdata", ""}, "/etc/netdata" },
+        { PathParts{"/etc/netdata///", ""}, "/etc/netdata" },
+        { PathParts{"/etc/netdata///", "health.d"}, "/etc/netdata/health.d" },
+        { PathParts{"/etc/netdata///", "///health.d"}, "/etc/netdata/health.d" },
+        { PathParts{"/etc/netdata", "///health.d"}, "/etc/netdata/health.d" },
+        { PathParts{"", "///health.d"}, "./health.d" },
+        { PathParts{"/", "///health.d"}, "/health.d" },
+    };
+
+    for (const auto &P : Values) {
+        struct PathParts PP = P.first;
+        const char *Expected = P.second;
+
+        char *Res = strdupz_path_subpath(PP.Path1, PP.Path2);
+        EXPECT_STREQ(Res, Expected);
+        freez(Res);
+    }
+}
