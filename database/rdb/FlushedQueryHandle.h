b8a9d73989a09ad948e2af5aab7da2eee4a8c886
Vasilis Kalintiris <vasilis@netdata.cloud>
2023-11-08 00:39:07 -0500

    Stashed

--- database/rdb/FlushedQueryHandle.h
+++ database/rdb/FlushedQueryHandle.h
@@ -10,6 +10,86 @@
 namespace rdb
 {
 
+class KeyQueryHandle
+{
+    friend class UniversalQuery;
+    
+public:
+    KeyQueryHandle(uint32_t GID, uint32_t MID, const IntervalManager<1024> &IM, uint32_t After, uint32_t Before)
+        : GID(GID), MID(MID), After(After), Before(Before), Now(After), Keys(IM.getKeys<16>(After, Before)), It(Keys.begin()), OP(), Finished(false)
+    {
+    }
+
+    [[nodiscard]] inline bool isFinished(pb::Arena &Arena)
+    {
+        if (!Finished)
+        {
+            if (OP.has_value() && (OP->first != OP->second))
+                return false;
+
+            Finished = !advance(Arena);
+        }
+
+        return Finished;
+    }
+
+    [[nodiscard]] inline STORAGE_POINT next()
+    {
+        if (OP->first == OP->second)
+            fatal("PageIterator already consumed");
+
+        Now++;
+        return *OP->first++;
+    }
+
+private:
+    [[nodiscard]] bool advance(pb::Arena &Arena)
+    {
+        while (It != Keys.end())
+        {
+            // Any old pages have been consumed. Reclaim space before
+            // creating a new one to keep memory consumption low.
+            Arena.Reset();
+
+            uint32_t PIT = *It;
+            Key K = Key(GID, MID, PIT);
+            rocksdb::PinnableSlice PSV;
+
+            // Use a MultiGet
+            Status S = SI->getMD(K.slice(), &PSV);
+            if (S.ok())
+            {
+                std::optional<Page> P = Page::deserialize(Arena, PSV.data());
+
+                if (P.has_value())
+                {
+                    OP = P->query(K.pit(), Now);
+                    if (OP.has_value())
+                    {
+                        It++;
+                        return true;
+                    }
+                }
+            }
+
+            It++;
+        }
+
+        return false;
+    }
+
+private:
+    uint32_t GID;
+    uint32_t MID;
+    uint32_t After;
+    uint32_t Before;
+    uint32_t Now;
+    absl::InlinedVector<uint32_t, 16> Keys;
+    absl::InlinedVector<uint32_t, 16>::iterator It;
+    std::optional<std::pair<Page::PageIterator, Page::PageIterator>> OP;
+    bool Finished;
+};
+
 class FlushedQueryHandle
 {
     friend class UniversalQuery;
