ea318ee36c7d9a7a07b02a9a6550517a07ef4025
Costa Tsaousis <costa@netdata.cloud>
2024-05-07 17:25:01 +0300

    support both MSYSTEM=MSYS and MSYSTEM=MINGW64 for configure

--- packaging/utils/clion-msys-msys-environment.bat
+++ packaging/utils/clion-msys-msys-environment.bat
@@ -9,11 +9,7 @@
 set "batch_dir=%~dp0"
 set "batch_dir=%batch_dir:\=/%"
 set MSYSTEM=MSYS
-
-:: go exists only mingw64 / ucrt64 / etc, not under msys profile
-set GOROOT=C:\msys64\mingw64
-
-set PATH="%PATH%;C:\msys64\usr\bin;C:\msys64\bin;C:\msys64\mingw64\bin"
+set PATH="%PATH%;C:\msys64\usr\bin;C:\msys64\bin"
 ::set PKG_CONFIG_EXECUTABLE=C:\msys64\mingw64\bin\pkg-config.exe
 ::set CMAKE_C_COMPILER=C:\msys64\mingw64\bin\gcc.exe
 ::set CMAKE_CC_COMPILER=C:\msys64\mingw64\bin\g++.exe
