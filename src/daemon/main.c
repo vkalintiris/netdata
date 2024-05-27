0c08bcf652bd81ba2c8cc3a83c034a24c9022775
vkalintiris <vasilis@netdata.cloud>
2024-05-27 13:54:16 +0300

    Run the agent as a Windows service.
    This commit contains the boilerplate code for running the agent as a
    Windows service.
    
    We start the agent's main as a separate thread, although this is not
    strictly required based on my experiments. We need similar logic for
    calling netdata's exit function when someone wants to stop the agent.
    
    However, at this point we need to resolve the issue of gaps when
    running the agent as a service. It seems that sleeping for one second
    with `sleep(1)`, actually sleeps for 2 to 4 seconds on my setup.
    
    Once we resolve this, the work that remains concerns packaging: ie.
    installing the binaries at the proper places so that the relevant
    DLLs are found.
    
    To test this PR you need to:
      - Build the agent: ./packaging/utils/compile-on-windows.sh
      - Install the files: `ninja -C build/ install`
      - Copy the main binary: `cp ./build/netdata /usr/bin/`
      - (Only once) Create the netdata service: `sc.exe config Netdata binPath="C:\msys64\usr\bin\netdata"`
      - Start the service: `sc.exe start Netdata`
    
    A couple notes:
      - The health and the spawn client have been disabled for the time
        being. They will be re-enabled once we finish the agent-as-service
        issue and the packaging.
      - Last time I checked, the agent crashes after a while when using
        dbengine. In order to have something that works correctly, you
        should specify memory-mode ram in your netdata.conf.

--- src/daemon/main.c
+++ src/daemon/main.c
@@ -1452,7 +1452,12 @@ int unittest_prepare_rrd(char **user) {
     return 0;
 }
 
-int main(int argc, char **argv) {
+#ifdef OS_WINDOWS
+int netdata_main(int argc, char **argv)
+#else
+int main(int argc, char **argv)
+#endif
+{
     // initialize the system clocks
     clocks_init();
     netdata_start_time = now_realtime_sec();
@@ -1463,10 +1468,15 @@ int main(int argc, char **argv) {
 
     int i;
     int config_loaded = 0;
-    int dont_fork = 0;
     bool close_open_fds = true;
     size_t default_stacksize;
     char *user = NULL;
+
+#ifdef OS_WINDOWS
+    int dont_fork = 1;
+#else
+    int dont_fork = 0;
+#endif
 
     static_threads = static_threads_get();
 
@@ -1481,6 +1491,7 @@ int main(int argc, char **argv) {
         exit(0);
     }
 
+    #if 0
     // parse options
     {
         int num_opts = sizeof(option_definitions) / sizeof(struct option_def);
@@ -1941,6 +1952,7 @@ int main(int argc, char **argv) {
             }
         }
     }
+    #endif
 
     if (close_open_fds == true) {
         // close all open file descriptors, except the standard ones
@@ -2206,7 +2218,10 @@ int main(int argc, char **argv) {
 
     // fork the spawn server
     delta_startup_time("fork the spawn server");
+
+#if 0
     spawn_init();
+#endif
 
     /*
      * Libuv uv_spawn() uses SIGCHLD internally:
