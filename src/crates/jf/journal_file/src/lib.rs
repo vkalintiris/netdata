f074f2a01c368a5e98efce2ecae300a49cb8108c
vkalintiris <vasilis@netdata.cloud>
2025-05-27 19:47:12 +0300

    Bumpity

--- src/crates/jf/journal_file/src/lib.rs
+++ src/crates/jf/journal_file/src/lib.rs
@@ -1,12 +1,12 @@
 mod hash;
-mod object;
 mod journal_file;
+mod object;
 pub mod offset_array;
 mod value_guard;
 
 pub use crate::hash::*;
 pub use error::Result;
+pub use journal_file::{EntryDataIterator, FieldDataIterator, FieldIterator, JournalFile};
 pub use memmap2::{Mmap, MmapMut};
 pub use object::*;
-pub use journal_file::{EntryDataIterator, FieldDataIterator, FieldIterator, JournalFile};
 pub use value_guard::ValueGuard;
