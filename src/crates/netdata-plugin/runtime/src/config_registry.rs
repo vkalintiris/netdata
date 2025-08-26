9547f08d8a4e9ee0f658927231d2cb940c3c4888
vkalintiris <vasilis@netdata.cloud>
2025-08-26 18:15:35 +0300

    Stashed

--- src/crates/netdata-plugin/runtime/src/config_registry.rs
+++ src/crates/netdata-plugin/runtime/src/config_registry.rs
@@ -1,4 +1,5 @@
 use crate::ConfigDeclaration;
+use netdata_plugin_protocol::DynCfgCmds;
 use schemars::{JsonSchema, SchemaGenerator, generate::SchemaSettings};
 use serde::{Serialize, de::DeserializeOwned};
 use std::collections::HashMap;
@@ -47,6 +48,18 @@ impl Config {
             }),
         }
     }
+
+    pub fn id(&self) -> &str {
+        &self.inner.declaration.id
+    }
+
+    pub fn schema(&self) -> &schemars::Schema {
+        &self.inner.schema
+    }
+
+    pub fn dyncfg_commands(&self) -> DynCfgCmds {
+        self.inner.declaration.cmds
+    }
 }
 
 #[derive(Default, Debug)]
@@ -59,10 +72,14 @@ impl ConfigRegistry {
         let inner = cfg.inner.clone();
         let id = inner.declaration.id.clone();
 
-        // insert cfg
         {
-            let mut hm = self.config_declarations.write().await;
-            hm.insert(id, cfg);
+            let mut guard = self.config_declarations.write().await;
+            guard.insert(id, cfg);
         }
+    }
+
+    pub async fn get(&self, id: &str) -> Option<Config> {
+        let guard = self.config_declarations.read().await;
+        guard.get(id).map(|x| x.clone())
     }
 }
