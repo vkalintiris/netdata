cb2682b9b512e7412637e5ce65a646e11eef61e2
vkalintiris <vasilis@netdata.cloud>
2025-10-19 23:37:26 +0300

    Bumpity

--- src/crates/journal/examples/index_state.rs
+++ src/crates/journal/examples/index_state.rs
@@ -1,3 +1,19 @@
+#![allow(unused_imports)]
+
+use journal::index_state::IndexState;
+use journal::registry::Registry;
+use journal::repository::File;
+
+use std::collections::HashSet;
+
 fn main() {
+    let mut registry = Registry::new().unwrap();
+    registry.watch_directory("/var/log/journal").unwrap();
+
+    let mut indexed_fields = HashSet::new();
+    indexed_fields.insert(String::from("PRIORITY"));
+
+    let mut index_state = IndexState::new(registry, indexed_fields);
+
     println!("Hello there!");
 }
