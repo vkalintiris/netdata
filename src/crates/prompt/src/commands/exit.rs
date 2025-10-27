67016a3fc7d9810348b81b1d88855e788b50bafc
vkalintiris <vasilis@netdata.cloud>
2025-10-27 20:58:37 +0200

    Bumpity

--- src/crates/prompt/src/commands/exit.rs
+++
@@ -1,6 +0,0 @@
-use std::io;
-
-pub fn execute(_args: &[String]) -> io::Result<()> {
-    // This is handled specially in the main loop
-    Ok(())
-}
--- src/crates/prompt/src/commands/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod clear;
-pub mod exit;
-pub mod help;
-pub mod ls;
-pub mod pwd;
-pub mod watch;
